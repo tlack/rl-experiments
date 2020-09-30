4c84f63b7d72995408911a5738811fe855aa53a0
tlack <lackner@gmail.com>
2020-09-29 23:20:02 -0400

    be less strict about is_notebook() determination

--- mypanda/envs/panda_env.py
+++ mypanda/envs/panda_env.py
@@ -14,12 +14,7 @@ import time
 def is_notebook():
     try:
         shell = get_ipython().__class__.__name__
-        if shell == "ZMQInteractiveShell":
-            return True  # Jupyter notebook or qtconsole
-        elif shell == "TerminalInteractiveShell":
-            return False  # Terminal running IPython
-        else:
-            return False  # Other type (?)
+        return True
     except NameError:
         return False  # Probably standard Python interpreter
 
